81dbba8f6fcd22f8d808a7bd97d0cf2bc7359152
Eric Yifu Liu <46115029+ericlau1987@users.noreply.github.com>
2023-04-22 23:56:34 +1000

    update the property class

---
+++ app/test.py
@@ -0,0 +1,11 @@
+from Property import Property
+
+property = Property(
+    no_street = 5,
+    street_name = "Longstaff",
+    street_type = "Road",
+    suburb = "Bayswater",
+    postcode = 3153
+)
+
+print(property.no_street)
