b967377d1b0cf0830d7873680635bcafe557c302
Eric Yifu Liu <46115029+ericlau1987@users.noreply.github.com>
2023-04-25 23:23:40 +1000

    add requirement

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional, List
 from random import randrange
 import models
@@ -7,10 +8,19 @@ import models
 from database import engine, SessionLocal, get_db
 from routers import post, user, property, auth, vote
 
-
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["https://www.google.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
